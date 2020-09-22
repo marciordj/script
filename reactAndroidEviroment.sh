ef53224dc26574ce5c196c7e3e960479a1a68d45
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-09-22 00:13:23 -0300

    Add flag -y

--- reactAndroidEviroment.sh
+++ reactAndroidEviroment.sh
@@ -1,9 +1,9 @@
 set -e
 
 echo "Java 8 install";
-sudo add-apt-repository ppa:openjdk-r/ppa
+sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jdk -y
 
 
 echo "End"
